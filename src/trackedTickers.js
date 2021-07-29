398f5c6f8b52291efbf8a1fdbafa613cccc2b865
avyukd <avyukd@gmail.com>
2021-07-29 12:15:08 -0400

    Added UR Energy

--- src/trackedTickers.js
+++ src/trackedTickers.js
@@ -3,7 +3,8 @@ const trackedTickers =
         {"ticker": "GLATF", "name": "Global Atomic"},
         {"ticker": "PALAF", "name": "Paladin Energy"},
         {"ticker": "BNNLF", "name": "Bannerman Energy"},
-        {"ticker":"DNN", "name": "Denison Mines"}
+        {"ticker":"DNN", "name": "Denison Mines"},
+        {"ticker":"URG","name":"UR Energy"}
     ]}
 
 export default trackedTickers; 
