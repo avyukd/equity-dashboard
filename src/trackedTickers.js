be1b86f2a64b4b5beefca0b600f9c3d7637a954a
avyukd <avyukd@gmail.com>
2021-07-29 11:48:14 -0400

    Added DNN

--- src/trackedTickers.js
+++ src/trackedTickers.js
@@ -2,7 +2,8 @@ const trackedTickers =
     {"uraniumTickers": [
         {"ticker": "GLATF", "name": "Global Atomic"},
         {"ticker": "PALAF", "name": "Paladin Energy"},
-        {"ticker": "BNNLF", "name": "Bannerman Energy"}
+        {"ticker": "BNNLF", "name": "Bannerman Energy"},
+        {"ticker":"DNN", "name": "Denison Mines"}
     ]}
 
 export default trackedTickers; 
