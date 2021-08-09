d4595c484cab3104e7dae3998ca77c772960d94d
avyukd <avyukd@gmail.com>
2021-08-08 22:48:21 -0400

    started modal overlay for semantic search

---
+++ src/components/SearchModal.js
@@ -0,0 +1,35 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure
+  } from "@chakra-ui/react"
+
+const SearchModal = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    
+    return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={2} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+    </Modal>);
+}
+
+export default SearchModal;
